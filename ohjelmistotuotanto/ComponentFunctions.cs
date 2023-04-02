5307778412b9833a578437380eea3de860199d77
Emil Frisk <wallaton100@gmail.com>
2023-04-02 08:26:57 +0300

    .

---
+++ ohjelmistotuotanto/ComponentFunctions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ohjelmistotuotanto
+{
+    internal class ComponentFunctions //test
+    {
+    }
+}
