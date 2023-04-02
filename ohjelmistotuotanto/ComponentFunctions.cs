25ad4eeb6b605bdc84761f099168a2253df0caab
Emil Frisk <wallaton100@gmail.com>
2023-04-02 08:43:54 +0300

    Added couple component creation functions

--- ohjelmistotuotanto/ComponentFunctions.cs
+++ ohjelmistotuotanto/ComponentFunctions.cs
@@ -6,7 +6,50 @@ using System.Threading.Tasks;
 
 namespace ohjelmistotuotanto
 {
-    internal class ComponentFunctions //test
+    class ComponentFunctions
     {
+
+        public static Button CreateButton(Control container, string text, Font font, int tabI, Point loc, Size sz, string name)
+        {
+            var button = new Button();
+            button.Text = text;
+            button.Font = font;
+            button.TabIndex = tabI;
+            button.Location = loc;
+            button.Size = sz;
+            button.Name = name;
+            container.Controls.Add(button);
+            return button;
+        }
+
+        public static Label CreateLabel(Control container, string name, Font font, int tabI, Point loc, string name2, bool isErrorLbl = false)
+        {
+            var label = new Label();
+            label.Font = font;
+            label.TabIndex = tabI;
+            label.Text = name;
+            label.Location = loc;
+            label.AutoSize = true;
+            label.Name = name2;
+
+            if (isErrorLbl)
+                label.ForeColor = Color.Red;
+
+            container.Controls.Add(label);
+
+            return label;
+        }
+
+        public static TextBox CreateTextBox(Control container, int tabI, Point loc, Size sz, string name)
+        {
+            var tb = new TextBox();
+            tb.TabIndex = tabI;
+            tb.Location = loc;
+            tb.Size = sz;
+            tb.Name = name;
+            container.Controls.Add(tb);
+
+            return tb;
+        }
     }
 }
