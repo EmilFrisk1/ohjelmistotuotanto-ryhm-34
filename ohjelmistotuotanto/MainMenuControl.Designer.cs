48fbf91fb7fdbcc89f35b02bc32b4cf1b57b9d98
Emil Frisk <wallaton100@gmail.com>
2023-04-11 11:27:55 +0300

    main menu layout ready

--- ohjelmistotuotanto/MainMenuControl.Designer.cs
+++ ohjelmistotuotanto/MainMenuControl.Designer.cs
@@ -30,8 +30,11 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainMenuControl));
             label1 = new Label();
-            button1 = new Button();
-            button2 = new Button();
+            reservationsBtn = new Button();
+            servicesBtn = new Button();
+            billsBtn = new Button();
+            customersBtn = new Button();
+            areasBtn = new Button();
             SuspendLayout();
             // 
             // label1
@@ -40,42 +43,83 @@
             label1.BackColor = Color.Transparent;
             label1.Font = new Font("Script MT Bold", 48F, FontStyle.Bold, GraphicsUnit.Point);
             label1.ForeColor = Color.Silver;
-            label1.Location = new Point(200, 17);
+            label1.Location = new Point(176, 17);
             label1.Name = "label1";
             label1.Size = new Size(451, 77);
             label1.TabIndex = 0;
             label1.Text = "Village Newbies";
             // 
-            // button1
+            // reservationsBtn
             // 
-            button1.BackColor = Color.DarkSlateGray;
-            button1.Font = new Font("Segoe UI", 27.75F, FontStyle.Regular, GraphicsUnit.Point);
-            button1.ForeColor = Color.Silver;
-            button1.Location = new Point(200, 136);
-            button1.Name = "button1";
-            button1.Size = new Size(451, 62);
-            button1.TabIndex = 1;
-            button1.Text = "button1";
-            button1.UseVisualStyleBackColor = false;
-            button1.Click += button1_Click;
+            reservationsBtn.BackColor = Color.DarkSlateGray;
+            reservationsBtn.Font = new Font("Segoe UI", 27.75F, FontStyle.Regular, GraphicsUnit.Point);
+            reservationsBtn.ForeColor = Color.Silver;
+            reservationsBtn.Location = new Point(176, 136);
+            reservationsBtn.Name = "reservationsBtn";
+            reservationsBtn.Size = new Size(451, 62);
+            reservationsBtn.TabIndex = 1;
+            reservationsBtn.Text = "Varaukset";
+            reservationsBtn.UseVisualStyleBackColor = false;
+            reservationsBtn.Click += button1_Click;
             // 
-            // button2
+            // servicesBtn
             // 
-            button2.Location = new Point(334, 318);
-            button2.Name = "button2";
-            button2.Size = new Size(75, 23);
-            button2.TabIndex = 2;
-            button2.Text = "button2";
-            button2.UseVisualStyleBackColor = true;
-            button2.Click += button2_Click;
+            servicesBtn.BackColor = Color.DarkSlateGray;
+            servicesBtn.Font = new Font("Segoe UI", 27.75F, FontStyle.Regular, GraphicsUnit.Point);
+            servicesBtn.ForeColor = Color.Silver;
+            servicesBtn.Location = new Point(176, 238);
+            servicesBtn.Name = "servicesBtn";
+            servicesBtn.Size = new Size(451, 62);
+            servicesBtn.TabIndex = 2;
+            servicesBtn.Text = "Palvelut";
+            servicesBtn.UseVisualStyleBackColor = false;
+            // 
+            // billsBtn
+            // 
+            billsBtn.BackColor = Color.DarkSlateGray;
+            billsBtn.Font = new Font("Segoe UI", 27.75F, FontStyle.Regular, GraphicsUnit.Point);
+            billsBtn.ForeColor = Color.Silver;
+            billsBtn.Location = new Point(177, 340);
+            billsBtn.Name = "billsBtn";
+            billsBtn.Size = new Size(451, 62);
+            billsBtn.TabIndex = 3;
+            billsBtn.Text = "Laskut";
+            billsBtn.UseVisualStyleBackColor = false;
+            // 
+            // customersBtn
+            // 
+            customersBtn.BackColor = Color.DarkSlateGray;
+            customersBtn.Font = new Font("Segoe UI", 27.75F, FontStyle.Regular, GraphicsUnit.Point);
+            customersBtn.ForeColor = Color.Silver;
+            customersBtn.Location = new Point(176, 442);
+            customersBtn.Name = "customersBtn";
+            customersBtn.Size = new Size(451, 62);
+            customersBtn.TabIndex = 4;
+            customersBtn.Text = "Asiakkaat";
+            customersBtn.UseVisualStyleBackColor = false;
+            // 
+            // areasBtn
+            // 
+            areasBtn.BackColor = Color.DarkSlateGray;
+            areasBtn.Font = new Font("Segoe UI", 27.75F, FontStyle.Regular, GraphicsUnit.Point);
+            areasBtn.ForeColor = Color.Silver;
+            areasBtn.Location = new Point(176, 544);
+            areasBtn.Name = "areasBtn";
+            areasBtn.Size = new Size(451, 62);
+            areasBtn.TabIndex = 5;
+            areasBtn.Text = "Alueet";
+            areasBtn.UseVisualStyleBackColor = false;
             // 
             // MainMenuControl
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
-            Controls.Add(button2);
-            Controls.Add(button1);
+            Controls.Add(areasBtn);
+            Controls.Add(customersBtn);
+            Controls.Add(billsBtn);
+            Controls.Add(servicesBtn);
+            Controls.Add(reservationsBtn);
             Controls.Add(label1);
             Name = "MainMenuControl";
             Size = new Size(804, 736);
@@ -86,7 +130,10 @@
         #endregion
 
         private Label label1;
-        private Button button1;
-        private Button button2;
+        private Button reservationsBtn;
+        private Button servicesBtn;
+        private Button billsBtn;
+        private Button customersBtn;
+        private Button areasBtn;
     }
 }
